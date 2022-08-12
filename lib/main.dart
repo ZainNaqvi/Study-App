271894ce0168a6b93884ef3b98c5cbc2414565f7
Zain Haider Naqvi <syedzainnaqvi3324@gmail.com>
2022-08-12 21:24:19 +0500

    Firebase Fire Cli Setup

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:study_app/screens/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home:HomePage(),
+    );
+  }
+}
